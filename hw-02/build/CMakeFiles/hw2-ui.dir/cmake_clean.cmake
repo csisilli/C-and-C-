92c8adb98a6ccd8a28ca8a39606bbfc5310412ed
Caitlin <74737672+csisilli@users.noreply.github.com>
2021-10-28 12:15:05 -0400

    Add files via upload

---
+++ hw-02/build/CMakeFiles/hw2-ui.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw2-ui.dir/main.cpp.o"
+  "hw2-ui"
+  "hw2-ui.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw2-ui.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ hw-02/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/cait/data/cmps231/code/hw-02
+# Build directory: /home/cait/data/cmps231/code/hw-02/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Tests "testing")
+set_tests_properties(Tests PROPERTIES  _BACKTRACE_TRIPLES "/home/cait/data/cmps231/code/hw-02/CMakeLists.txt;8;add_test;/home/cait/data/cmps231/code/hw-02/CMakeLists.txt;0;")
