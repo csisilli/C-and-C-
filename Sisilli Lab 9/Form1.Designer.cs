84766ceed0e26c5bf481c348905173d8a900b542
Caitlin Sisilli <74737672+csisilli@users.noreply.github.com>
2022-12-01 10:41:27 -0500

    Add files via upload

---
+++ Sisilli Lab 9/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Sisilli_Lab_9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.patternText = new System.Windows.Forms.TextBox();
+            this.stringText = new System.Windows.Forms.TextBox();
+            this.outputText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // patternText
+            // 
+            this.patternText.Location = new System.Drawing.Point(205, 68);
+            this.patternText.Name = "patternText";
+            this.patternText.Size = new System.Drawing.Size(371, 26);
+            this.patternText.TabIndex = 0;
+            // 
+            // stringText
+            // 
+            this.stringText.Location = new System.Drawing.Point(205, 143);
+            this.stringText.Name = "stringText";
+            this.stringText.Size = new System.Drawing.Size(371, 26);
+            this.stringText.TabIndex = 1;
+            // 
+            // outputText
+            // 
+            this.outputText.Location = new System.Drawing.Point(188, 286);
+            this.outputText.Multiline = true;
+            this.outputText.Name = "outputText";
+            this.outputText.ReadOnly = true;
+            this.outputText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputText.Size = new System.Drawing.Size(400, 100);
+            this.outputText.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter a String ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(314, 237);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "The output is:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(67, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Enter a Pattern";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outputText);
+            this.Controls.Add(this.stringText);
+            this.Controls.Add(this.patternText);
+            this.Name = "Form1";
+            this.Text = "Lab 9";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox patternText;
+        private System.Windows.Forms.TextBox stringText;
+        private System.Windows.Forms.TextBox outputText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Sisilli Lab 9/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sisilli_Lab_9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void findSplit()
+        {
+            outputText.Text = "";
+            string pattern = patternText.Text;
+            string strings = stringText.Text;
+            string[] args = Regex.Split(strings, pattern);
+            for (int i = 0; i < args.Length; i++)
+            {
+                outputText.Text += ((i + 1) + "  The String is: " + args[i]);
+            }
+        }
+        private void patternText_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) return;
+            findSplit();
+        }
+        private void stringText_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) return;
+            findSplit();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
