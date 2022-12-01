d65e9588caa56c6e2bf2059710c44fa7c8c6bdd1
Caitlin Sisilli <74737672+csisilli@users.noreply.github.com>
2022-12-01 10:41:02 -0500

    Add files via upload

---
+++ Sisilli Lab 8/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+//Lab 8
+//Name:Caitlin Sisilli
+
+namespace Sisilli_Lab_8
+{
+    internal class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            SortedDictionary<String,
+                             int> words= new SortedDictionary<String,
+                                                            int>();
+            var str = File.ReadAllText("SisilliLab8.txt");
+            var wordFreq = new Regex(@"\w+");
+            
+            foreach (Match match in wordFreq.Matches(str))
+            {
+                int currentCount = 0;
+                words.TryGetValue(match.Value, out currentCount);
+
+                currentCount++;
+                words[match.Value] = currentCount;
+            }
+            Console.WriteLine(words);
+        }
+    }
+}
+
+    
+
+
