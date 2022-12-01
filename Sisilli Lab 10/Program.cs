a6721ff6298828381bd372a193b9aa37d05094d9
Caitlin Sisilli <74737672+csisilli@users.noreply.github.com>
2022-12-01 10:41:42 -0500

    Add files via upload

---
+++ Sisilli Lab 10/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Sisilli_Lab_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int content= args.Length;
+            if (content == 0)
+            {
+                Console.WriteLine("No FileName Found");
+                return;
+            }
+            Process[] proces = new Process[content];
+            for(int i = 0; i<content; i++)
+            {
+                proces[i] = new Process();
+                proces[i].StartInfo.FileName = args[i];
+                proces[i].Start();
+            }
+            while (true)
+            {
+                proces[0].WaitForExit(1000);
+                for(int i = 0; i < content; i++)
+                {
+                    if (proces[i].HasExited)
+                    {
+                        proces[i].Start();
+                    }
+                }
+            }
+        }
+    }
+}
